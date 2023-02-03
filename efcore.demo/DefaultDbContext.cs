e037cb94ae52ace300c884a4c1da4fdfbeca256f
crazy xin <804200154@qq.com>
2023-02-03 22:05:28 +0800

    Update DefaultDbContext.cs

--- efcore.demo/DefaultDbContext.cs
+++ efcore.demo/DefaultDbContext.cs
@@ -5,11 +5,8 @@ namespace efcore.demo;
 public class DefaultDbContext : DbContext
 {
     public DbSet<AppLoginLog> AppLoginLogs { get; set; }
-
-    // private readonly string connectionString = @"Server=10.98.59.35;port=31791;database=db_mutone0001;user id=root;
-    // password=fangte123";
     
-    private readonly string connectionString = @"Server=10.98.98.183;port=3306;database=testdemo;user id=root;
+    private readonly string connectionString = @"Server=127.0.0.1;port=3306;database=testdemo;user id=root;
     password=123456";
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
